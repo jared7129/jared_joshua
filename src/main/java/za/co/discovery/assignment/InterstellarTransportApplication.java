17797f5e6c8cf1da38850ac1dfae8e53ae144166
Jared Joshua <jared@propdata.net>
2020-05-05 22:56:16 +0200

    Update InterstellarTransportApplication.java

--- src/main/java/za/co/discovery/assignment/InterstellarTransportApplication.java
+++ src/main/java/za/co/discovery/assignment/InterstellarTransportApplication.java
@@ -3,6 +3,12 @@ package za.co.discovery.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Jared Joshua
+ * Uses Spring
+ */
+
 @SpringBootApplication
 public class InterstellarTransportApplication {
 
